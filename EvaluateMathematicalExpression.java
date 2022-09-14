3a460b307628a39a2e19bf56e78ca6d49f88eeb9
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-14 22:38:34 +0300

    Add files via upload

--- EvaluateMathematicalExpression.java
+++ EvaluateMathematicalExpression.java
@@ -7,7 +7,7 @@ public class EvaluateMathematicalExpression {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String expression = "2/(2+3+1*5)*4-6";
+        String expression = "1+1";
         System.out.println(findSum(expression));
 
     }
@@ -34,36 +34,89 @@ public class EvaluateMathematicalExpression {
                 i--;
                 String expressionSubstring = expressionToDelete.substring(1, expressionToDelete.length() - 1);///-1
                 double sum = findSum(expressionSubstring);
+
                 queue.offer(String.valueOf(sum));
             } else {
                 queue.offer(String.valueOf(expression.charAt(i)));
             }
         }
+        return doTheOperations(queue);
+    }
 
+    private static double doTheOperations(ArrayDeque<String> queue) {
 
         double firstNumber = Double.parseDouble(queue.poll());
         double totalSum = firstNumber;
+
+        ArrayDeque<String> toDoAfter = new ArrayDeque<>();
+
+        int count = 0;
         while (!queue.isEmpty()) {
             char operator = queue.poll().charAt(0);
             double nextNumber = Double.parseDouble(queue.poll());
 
             switch (operator) {
                 case '+':
-                    totalSum = firstNumber + nextNumber;
+                    if (checkForDivisionOrMultiplication(queue)) {
+                        if(toDoAfter.isEmpty()){
+                            toDoAfter.offer(String.valueOf(firstNumber));
+                        }
+                        toDoAfter.offer(String.valueOf(operator));
+                        toDoAfter.offer(String.valueOf(nextNumber));
+                        firstNumber = Double.parseDouble(toDoAfter.peek());
+                    } else {
+                        totalSum = firstNumber + nextNumber;
+                        firstNumber = totalSum;
+                    }
                     break;
                 case '-':
-                    totalSum = firstNumber - nextNumber;
+                    if (checkForDivisionOrMultiplication(queue)) {
+                        if(toDoAfter.isEmpty()){
+                            toDoAfter.offer(String.valueOf(firstNumber));
+                        }
+                        toDoAfter.offer(String.valueOf(operator));
+                        toDoAfter.offer(String.valueOf(nextNumber));
+                        firstNumber = Double.parseDouble(toDoAfter.peek());
+                    } else {
+                        totalSum = firstNumber - nextNumber;
+                        firstNumber = totalSum;
+                    }
                     break;
                 case '*':
-                    totalSum = firstNumber * nextNumber;
+                    if(!toDoAfter.isEmpty()){
+                        firstNumber = Double.parseDouble(toDoAfter.pollLast());
+                        totalSum = firstNumber * nextNumber;
+                        toDoAfter.offer(String.valueOf(totalSum));
+                    } else {
+                        totalSum = firstNumber * nextNumber;
+                        firstNumber = totalSum;
+                    }count++;
                     break;
                 case '/':
-                    totalSum = firstNumber / nextNumber;
+                    if(!toDoAfter.isEmpty()){
+                        firstNumber = Double.parseDouble(toDoAfter.pollLast());
+                        totalSum = firstNumber / nextNumber;
+                        toDoAfter.offer(String.valueOf(totalSum));
+                    } else {
+                        totalSum = firstNumber * nextNumber;
+                        firstNumber = totalSum;
+                    }count++;
                     break;
-
             }
-            firstNumber = totalSum;
+
         }
-        return totalSum;
+        if(toDoAfter.isEmpty()){
+            return totalSum;
+        }
+        return doTheOperations(toDoAfter);
     }
+
+    private static boolean checkForDivisionOrMultiplication(ArrayDeque<String> queue) {
+        if (queue.contains("*") || queue.contains("/")) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
