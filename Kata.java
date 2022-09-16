3c8746f7b0ca033473bf9603d73771ca7cefbe90
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-16 23:13:52 +0300

    Add files via upload

---
+++ Kata.java
@@ -0,0 +1,29 @@
+package com.company.Tasks;
+
+public class Kata {
+
+    public static int nextHigher(int n) {
+
+        int sum = findSumOfDigitsInBinaryNumber(n);
+
+        int sumNextNumberWithSameBinary = 0;
+        int nextNumber = n + 1;
+        while (sum != sumNextNumberWithSameBinary) {
+            sumNextNumberWithSameBinary = findSumOfDigitsInBinaryNumber(nextNumber);
+            if (sumNextNumberWithSameBinary == sum) {
+                return nextNumber;
+            }
+            nextNumber++;
+        }
+        return 0;
+    }
+
+    public static int findSumOfDigitsInBinaryNumber(int number) {
+        String numberToBinary = Integer.toBinaryString(number);
+        int sum = 0;
+        for (int i = 0; i < numberToBinary.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(numberToBinary.charAt(i)));
+        }
+        return sum;
+    }
+}
