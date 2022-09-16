421c250971684d6239bafc58367a9227c1f2b061
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-16 23:14:24 +0300

    Add files via upload

--- Tasks/Main.java
+++ Tasks/Main.java
@@ -4,18 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-       System.out.println(Kata.nextHigher(127));
-       System.out.println(Dinglemouse.peakHeight(new char[][]{
-               "^^^^^^        ".toCharArray(),
-               " ^^^^^^^^     ".toCharArray(),
-               "  ^^^^^^^     ".toCharArray(),
-               "  ^^^^^       ".toCharArray(),
-               "  ^^^^^^^^^^^ ".toCharArray(),
-               "  ^^^^^^      ".toCharArray(),
-               "  ^^^^        ".toCharArray()
-       }));
-       MathEvaluator mathEvaluator = new MathEvaluator();
-       System.out.println(mathEvaluator.calculate("1--6"));
+        System.out.println(Kata.nextHigher(127));
+        System.out.println(Dinglemouse.peakHeight(new char[][]{
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^ ".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+        }));
+        MathEvaluator mathEvaluator = new MathEvaluator();
+        System.out.println(mathEvaluator.calculate("6+(-2)"));
 
 
     }
