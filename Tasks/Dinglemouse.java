5e990970826204aadf8275d59e27d1349e812273
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-16 19:53:53 +0300

    Add files via upload

--- Tasks/Dinglemouse.java
+++ Tasks/Dinglemouse.java
@@ -7,25 +7,29 @@ public class Dinglemouse {
 
         while (matrixHasChars(mountain)) {
             height++;
-            for (int column = 0; column < 14; column++) {
-                for (int row = 0; row < 7; row++) {
+            for (int row = 0; row < mountain.length; row++) {
+                char previousChar = ' ';
+                for (int column = 0; column < mountain[row].length; column++) {
                     char charToCheck = mountain[row][column];
                     if (charToCheck == '^') {
-                        if (checkIfCharIsNotCovered(row, column, mountain, height)) {
-                            mountain[row][column] = String.valueOf(height).charAt(0);
+                        if (checkIfCharIsNotCovered(row, column, mountain, height,previousChar)) {
+                            if (height > 9) {
+                                mountain[row][column] = String.valueOf(height).charAt(String.valueOf(height).length()-1);
+                            } else {
+                                mountain[row][column] = String.valueOf(height).charAt(0);
+                            }
                         }
                     }
+                    previousChar = charToCheck;
                 }
             }
-
-
         }
         return height;
     }
 
     private static boolean matrixHasChars(char[][] mountain) {
-        for (int row = 0; row < 7; row++) {
-            for (int cols = 0; cols < 14; cols++) {
+        for (int row = 0; row < mountain.length; row++) {
+            for (int cols = 0; cols < mountain[row].length; cols++) {
                 char field = mountain[row][cols];
                 if (field == '^') {
                     return true;
@@ -35,12 +39,12 @@ public class Dinglemouse {
         return false;
     }
 
-    private static boolean checkIfCharIsNotCovered(int row, int column, char[][] mountain, int height) {
+    private static boolean checkIfCharIsNotCovered(int row, int column, char[][] mountain, int height, char previousChar) {
         char charToCheck = mountain[row][column];
         if (charToCheck == ' ') {
             return false;
         }
-        if (column == 0 || column == 13 || row == 0 || row == 6) {
+        if (column == 0 || column == mountain[row].length - 1 || row == 0 || row == mountain.length - 1) {
             return true;
         }
 
@@ -49,19 +53,26 @@ public class Dinglemouse {
         char charUp = mountain[row][column - 1];
         char charDown = mountain[row][column + 1];
 
-        if (checkTheChar(charOnTheLeft, height) && checkTheChar(charOnTheRight, height)
-                && checkTheChar(charUp, height) && checkTheChar(charDown, height)) {
+        if (checkTheChar(charOnTheLeft, height, previousChar) && checkTheChar(charOnTheRight, height, previousChar)
+                && checkTheChar(charUp, height, previousChar) && checkTheChar(charDown, height, previousChar)) {
             return false;
         }
         return true;
     }
 
-    private static boolean checkTheChar(char charToCheck, int height) {
+    private static boolean checkTheChar(char charToCheck, int height, char previousChar) {
         if (charToCheck == ' ') {
             return false;
-        }
-        if (charToCheck == '^' || Integer.parseInt(String.valueOf(charToCheck)) == height) {
+        } if(charToCheck == previousChar && charToCheck == '^'){
             return true;
+        }
+        if(height<= 9){
+            if (charToCheck == '^' || Integer.parseInt(String.valueOf(charToCheck)) == height) {
+                return true;
+            }} else {
+            if (charToCheck == '^' || charToCheck == String.valueOf(height).charAt(String.valueOf(height).length() - 1)) {
+                return true;
+            }
         }
         return false;
     }
