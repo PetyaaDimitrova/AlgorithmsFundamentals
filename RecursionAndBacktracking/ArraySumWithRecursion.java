9aa59fd4c93571ea6598cafee584356e02d55489
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-09-27 12:53:44 +0300

    Recursion

---
+++ RecursionAndBacktracking/ArraySumWithRecursion.java
@@ -0,0 +1,24 @@
+package RecursionAndBacktracking;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySumWithRecursion {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scan.nextLine().split(" "))
+                        .mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(sum(numbers, 0));
+
+    }
+
+    private static int sum(int[] arr, int index) {
+        if(index >= arr.length){
+            return 0;
+        }
+        return arr[index] + sum(arr, index + 1);
+    }
+}
