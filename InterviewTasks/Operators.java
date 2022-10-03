d3a1ac5fadd0edf4af45a32b98e25be818e7d9c3
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-03 20:09:34 +0300

    Update Operators.java

--- InterviewTasks/Operators.java
+++ InterviewTasks/Operators.java
@@ -10,6 +10,7 @@ public class Operators {
         int x = Integer.parseInt(scan.nextLine());
         int y = Integer.parseInt(scan.nextLine());
 
+        
         //!x || !y || y       =>  !(x && y) is the same like !x || !y , but we have also (!x  || y) and (!y || y).
         // We already have !x and !y and we need to add 'y' without '!' in our expression.
     }
