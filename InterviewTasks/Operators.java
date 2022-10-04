618157d50a9994539a8dd65d1e2cee2b9f87ed3e
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-04 12:02:49 +0300

    Update Operators.java

--- InterviewTasks/Operators.java
+++ InterviewTasks/Operators.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 public class Operators {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int x = Integer.parseInt(scan.nextLine());
-        int y = Integer.parseInt(scan.nextLine());
-
-        
+      
     }
 }
