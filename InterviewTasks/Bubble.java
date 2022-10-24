cbda728129083b886979fdeac5037fcf6d19310a
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-24 13:02:50 +0300

    Sorting algorithms

--- InterviewTasks/Bubble.java
+++ InterviewTasks/Bubble.java
@@ -1,34 +1,29 @@
 package InterviewTasks;
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-
+public class Bubble {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
+
         int[] numbers = Arrays.stream(scan.nextLine().split(" "))
                         .mapToInt(Integer::parseInt).toArray();
 
 
-
-        for(int i = 0; i<numbers.length; i++){
-            int minNumberIndex = i;
-            for(int j = i+1; j< numbers.length; j++){
-                if(numbers[j] < numbers[minNumberIndex]){
-                    minNumberIndex = j;
+        for(int i = 0; i < numbers.length; i++){
+            int minNumber = numbers[i];
+            for (int j = i+1; j <  numbers.length; j++) {
+                if(numbers[j]<minNumber){
+                    swap(numbers, i, j);
                 }
             }
-            swap(numbers, minNumberIndex, i);
         }
         for (int n:numbers
              ) {
             System.out.print(n + " ");
         }
     }
-
     private static void swap(int[] numbers, int minNumberIndex, int i) {
         int temp = numbers[minNumberIndex];
         numbers[minNumberIndex] = numbers[i];
